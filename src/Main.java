6a81431d469d0ab67f3275f8b977a3561360767e
Ján VALÁŠEK <valasek@tipsport.cz>
2024-04-02 14:17:38 +0200

    Frist commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int [] numbers = new int[10];
+
+//        for (int i =0; i < numbers.length; i++){
+//            numbers[i] = i;
+//            System.out.println(numbers[i]);
+git//        }
+
+//        for (int number : numbers){
+//            System.out.println(number);
+//        }
+
+        String [] names = new String[] {"naka", "piko", "mioko"};
+        for (String name : names){
+            System.out.println(name);
+        }
+    }
+}
